c8b81942d190d2dc079a599ac6cb63bfacf17396
TERRAL Ludovic <ludovic.terral@dev.lan>
2020-08-25 10:52:37 +0200

    correction entete

--- lixbox-crypto/src/test/java/fr/lixbox/security/crypto/util/AESUtilTest.java
+++ lixbox-crypto/src/test/java/fr/lixbox/security/crypto/util/AESUtilTest.java
@@ -3,14 +3,14 @@
  *                           FRAMEWORK Lixbox
  *                          ==================
  *      
- * This file is part of lixbox-plugins.
+ *    This file is part of lixbox-security.
  *
  *    lixbox-security is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
  *    the Free Software Foundation, either version 3 of the License, or
  *    (at your option) any later version.
  *
- *    lixbox-supervision is distributed in the hope that it will be useful,
+ *    lixbox-security is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *    GNU General Public License for more details.
